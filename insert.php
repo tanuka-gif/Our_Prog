feafbc57b926eff53fe2554015667d346d312997
tanuka-gif <tanuka.sinha.hit@gmail.com>
2024-08-14 12:23:02 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -7,15 +7,28 @@
 <body>
     <center>
         <?php
-
+$host = 'database-1.c5u8ksk2gp91.ap-south-1.rds.amazonaws.com'; // Replace with your RDS endpoint
+$port = '3306'; // Default MySQL port
+$dbname = 'user2'; // Replace with your database name
+$username = 'admin'; // Replace with your database username
+$password = '1234tanu'; // Replace with your database password
         
-        $conn = mysqli_connect("localhost", "root", "", "user2");
+        //*$conn = mysqli_connect("localhost", "root", "", "user2");
 
         // Check connection
-        if($conn === false){
-            die("Could not connect. "
-                . mysqli_connect_error());
-        }
+        //if($conn === false){
+         //   die("Could not connect. "
+          //      . mysqli_connect_error());
+       // }
+// Create a connection
+$mysqli = new mysqli($host, $username, $password, $dbname, $port);
+
+// Check the connection
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+} else {
+    echo "Connected successfully!";
+}
 
         // Taking all 5 values from the form data(input)
         $name = $_REQUEST['name'];
